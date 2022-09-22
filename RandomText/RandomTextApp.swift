fb8c99d76dadc90eb90689de9250e035457c7dc5
jake <palatable7@naver.com>
2022-09-23 00:56:07 +0900

    Initial Commit

---
+++ RandomText/RandomTextApp.swift
@@ -0,0 +1,17 @@
+//
+//  RandomTextApp.swift
+//  RandomText
+//
+//  Created by 김종권 on 2022/09/23.
+//
+
+import SwiftUI
+
+@main
+struct RandomTextApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
